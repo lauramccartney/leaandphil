33a38c2b3aa4f227da526a69c9ae4e3ff1c82905
Laura McCartney <hello@lauramccartney.co.uk>
2013-09-19 16:17:07 +0100

    Get snappish containers to activate

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
 $('#mywrapper').snappish();
+
+$('#mywrapper').on('scrollbegin.snappish', function(event, data) {
+  console.log('from slide', data.fromSlide, data.fromSlideNum);
+  console.log('to slide', data.toSlide, data.toSlideNum);
+  data.fromSlide.removeClass('active');
+  data.toSlide.addClass('active');
+});
