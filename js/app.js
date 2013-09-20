94f852f556f86572f9bf50a21d326e5a47b765fa
Laura McCartney <hello@lauramccartney.co.uk>
2013-09-20 17:47:55 +0100

    Scroll animations works! :-)

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
 $('#mywrapper').snappish();
 
 $('#mywrapper').on('scrollbegin.snappish', function(event, data) {
-  console.log('from slide', data.fromSlide, data.fromSlideNum);
-  console.log('to slide', data.toSlide, data.toSlideNum);
   data.fromSlide.removeClass('active');
   data.toSlide.addClass('active');
 });
