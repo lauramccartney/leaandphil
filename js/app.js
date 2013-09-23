928b1fb9d81c82f237353efc9eb2cb3e27d4b974
Laura McCartney <hello@lauramccartney.co.uk>
2013-09-23 13:33:12 +0100

    Add images and columns and such

--- js/app.js
+++ js/app.js
@@ -4,3 +4,18 @@ $('#mywrapper').on('scrollbegin.snappish', function(event, data) {
   data.fromSlide.removeClass('active');
   data.toSlide.addClass('active');
 });
+
+var $snappish = $('#wrapper').snappish();
+
+$snappish.on('scrollbegin.snappish', function(e, data) {
+    data.toSlide.addClass('active');
+    $('nav a').removeClass('active');
+    $('nav a[data-slide-num="' + data.toSlideNum + '"]').addClass('active');
+}).on('scrollend.snappish', function(e, data) {
+    data.fromSlide.removeClass('active');
+});
+
+$('nav a').on('click', function(e) {
+    e.preventDefault();
+    $snappish.trigger('scrollto.snappish', $(this).data('slide-num'));
+});
